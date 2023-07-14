1646f80fd626c20586e72e79afc721fb451bbb7c
vipinsunny <vipinsunny96@gmail.com>
2023-07-14 08:27:11 +0530

    changed url

--- client/src/pages/CreatePost.js
+++ client/src/pages/CreatePost.js
@@ -17,14 +17,11 @@ export default function CreatePost() {
     data.set("content", content);
     data.set("file", files[0]);
     ev.preventDefault();
-    const response = await fetch(
-      "https://myblog-mern-blog-website.onrender.com/post",
-      {
-        method: "POST",
-        body: data,
-        credentials: "include",
-      }
-    );
+    const response = await fetch("http://localhost:4000/post", {
+      method: "POST",
+      body: data,
+      credentials: "include",
+    });
     if (response.ok) {
       setRedirect(true);
     }
